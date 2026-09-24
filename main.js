8d07f708c1ce83a467998e93e68ee18be6316420
Ashish Tiwari <160581153+Ashish2k04@users.noreply.github.com>
2026-09-24 22:45:38 +0530

    Character Splitter logic implimented to divide documents into multiple small chunk documents

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 
 const loader = new PDFLoader("./story.pdf")
 
 const docs = await loader.load()
 
-console.log(docs)
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 10,
+    chunkOverlap: 0
+});
+
+const chunks = await splitter.splitDocuments(docs)
+
+console.log(chunks)
