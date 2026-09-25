3d125c5294fe25ab281f13e21b117785739d7b88
Ashish Tiwari <160581153+Ashish2k04@users.noreply.github.com>
2026-09-25 20:24:36 +0530

    Again changed the code order

--- main.js
+++ main.js
@@ -4,23 +4,23 @@ import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 import { MistralAIEmbeddings } from "@langchain/mistralai";
 import { Pinecone } from '@pinecone-database/pinecone';
 
-const loader = new PDFLoader("./story.pdf")
-
-const data = await loader.load()
+const embeddings = new MistralAIEmbeddings({
+   apiKey: process.env.MISTRAL_API_KEY,
+   model: "mistral-embed"
+})
 
 const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 500,
     chunkOverlap: 0
 });
 
+const loader = new PDFLoader("./story.pdf")
+
+const data = await loader.load()
+
 const chunks = await splitter.splitText(
     data.map((doc) => doc.pageContent).join("\n")
 )
-
-const embeddings = new MistralAIEmbeddings({
-   apiKey: process.env.MISTRAL_API_KEY,
-   model: "mistral-embed"
-})
 
 const docs = await Promise.all(chunks.map(async (chunk) => {
  const embedding = await embeddings.embedQuery(chunk);
