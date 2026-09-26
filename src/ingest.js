3ae241d9cde41e016902f7495c51cc7127c9dc54
Ashish Tiwari <160581153+Ashish2k04@users.noreply.github.com>
2026-09-26 16:29:34 +0530

    Removed all commented codes

--- src/ingest.js
+++ src/ingest.js
@@ -9,10 +9,10 @@ const embeddings = new MistralAIEmbeddings({
    model: "mistral-embed"
 })
 
-// const splitter = new RecursiveCharacterTextSplitter({
-//     chunkSize: 500,
-//     chunkOverlap: 0
-// });
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 0
+});
 
 const pc = new Pinecone({
     apiKey: process.env.PINECONE_API_KEY
@@ -20,38 +20,33 @@ const pc = new Pinecone({
 
 const index = pc.index('rag-test')
 
-// const loader = new PDFLoader("./story.pdf")
+const loader = new PDFLoader("./story.pdf") //Write any pdf file path here to load
 
-// const data = await loader.load()
+const data = await loader.load()
 
-// const chunks = await splitter.splitText(
-//     data.map((doc) => doc.pageContent).join("\n")
-// )
+const chunks = await splitter.splitText(
+    data.map((doc) => doc.pageContent).join("\n")
+)
 
-// const docs = await Promise.all(chunks.map(async (chunk) => {
-//  const embedding = await embeddings.embedQuery(chunk);
-//  return {
-//     text: chunk,
-//     embedding
-//  }
-// }))
+const docs = await Promise.all(chunks.map(async (chunk) => {
+ const embedding = await embeddings.embedQuery(chunk);
+ return {
+    text: chunk,
+    embedding
+ }
+}))
 
-// const result = await index.upsert({
-//     records: docs.map((doc, i) => ({
-//         id: `doc-${i}`,
-//         values: doc.embedding,
-//         metadata: {
-//             text: doc.text
-//         }
-//     }))
-// })
-
-const queryEmbeding = await embeddings.embedQuery("Who is Pebble in the story?")
-
-const result = await index.query({
-    vector: queryEmbeding,
-    topK: 2,
-    includeMetadata: true
+const result = await index.upsert({
+    records: docs.map((doc, i) => ({
+        id: `doc-${i}`,
+        values: doc.embedding,
+        metadata: {
+            text: doc.text
+        }
+    }))
 })
 
-console.log(JSON.stringify(result));
+console.log(result);
+
+
+
